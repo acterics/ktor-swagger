5e282495bd27b56e5f8d27e49acf2f3f8a36d382
Oleg Lipskiy <lolego1601@gmail.com>
2018-12-17 23:11:45 +0200

    build: update version

--- ktor-swagger-root.gradle.kts
+++ ktor-swagger-root.gradle.kts
@@ -32,7 +32,7 @@ allprojects {
         plugin("com.diffplug.gradle.spotless")
     }
     group = "de.nielsfalk.ktor"
-    version = "0.3.1"
+    version = "0.4"
 
     repositories {
         mavenCentral()
